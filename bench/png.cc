muhammedkaraarslan/veccore

commit 56c7d74778111fe79f46efecf36cfa4ac1ea2031

Ensure variables are considered initialized by the compiler

bench/png.cc
@@ -60,22 +60,25 @@ void colormap(int c, int& cr, int& cg, int& cb)
 
 void write_png(const char *filename, unsigned char *data, size_t nx, size_t ny)
 {
-    FILE *output;
-    gdImagePtr image;
-    int colors[256];
+    FILE *output = fopen(filename, "wb");
 
-    if (!(output = fopen(filename, "wb"))) {
+    if (!output) {
         fprintf(stderr, "Error: cannot open file %s\n", filename);
         return;
     }
 
-    if (!(image = gdImageCreate(nx, ny))) {
+    gdImagePtr image = gdImageCreate(nx, ny);
+
+    if (!image) {
         fprintf(stderr, "Error: cannot create image\n");
+        fclose(output);
         return;
     }
 
+    int colors[256];
+
     for (size_t i = 0; i < 256; ++i) {
-        int r, g, b;
+        int r = 0, g = 0, b = 0;
         colormap(i, r, g, b);
         colors[i] = gdImageColorAllocate(image, r, g, b);
     }
@@ -91,16 +94,18 @@ void write_png(const char *filename, unsigned char *data, size_t nx, size_t ny)
 
 void write_png(const char *filename, Color *data, size_t nx, size_t ny)
 {
-    FILE *output;
-    gdImagePtr image;
+    FILE *output = fopen(filename, "wb");
 
-    if (!(output = fopen(filename, "wb"))) {
+    if (!output) {
         fprintf(stderr, "Error: cannot open file %s\n", filename);
         return;
     }
 
-    if (!(image = gdImageCreateTrueColor(nx, ny))) {
+    gdImagePtr image = gdImageCreate(nx, ny);
+
+    if (!image) {
         fprintf(stderr, "Error: cannot create image\n");
+        fclose(output);
         return;
     }
 

bench/quadratic.cc
@@ -273,15 +273,12 @@ void TestQuadSolve(const float *__restrict__ a, const float *__restrict__ b, con
 
 int main()
 {
-  float *a, *b, *c, *x1, *x2;
-  int *roots;
-
-  a     = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
-  b     = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
-  c     = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
-  x1    = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
-  x2    = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
-  roots = (int *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(int));
+  float *a   = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
+  float *b   = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
+  float *c   = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
+  float *x1  = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
+  float *x2  = (float *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(float));
+  int *roots = (int *)AlignedAlloc(VECCORE_SIMD_ALIGN, kN * sizeof(int));
 
   srand((unsigned)time(NULL));
 

include/VecCore/Backend/ScalarWrapper.h
@@ -93,7 +93,7 @@ public:
   void store(Bool_s *dest) const { *dest = fBool; }
 
 private:
-  Bool_s fBool;
+  Bool_s fBool{};
 };
 
 template <class T>

commit eef6582c835f651e3bd1d83455e02a4408722941

Fix bug in write_png() for true color image
Fixes: 56c7d74778111fe79f46efecf36cfa4ac1ea2031

bench/png.cc
@@ -101,7 +101,7 @@ void write_png(const char *filename, Color *data, size_t nx, size_t ny)
         return;
     }
 
-    gdImagePtr image = gdImageCreate(nx, ny);
+    gdImagePtr image = gdImageCreateTrueColor(nx, ny);
 
     if (!image) {
         fprintf(stderr, "Error: cannot create image\n");